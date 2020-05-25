cb6f9fc9df5575c1bb36b598b931c80815273a29
VarspTheDev <65226823+VarspTheDev@users.noreply.github.com>
2020-05-25 21:38:03 +1000

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ bot.on('message', message => {
             {name: 'Reaction Roles', value:  '`-help Reaction`', inline: true }, 
             {name: 'Music', value: '`-help Music`', inline: true }, 
             {name: 'Commands', value: '`-help Commands`', inline: true }, 
-            {name: 'Extra Support', value: 'Join Our DiSscord - '}
+            {name: 'Extra Support', value: 'Join Our DiSscord -discord.gg/kwC5QAx '}
         )  
         .setColor(0x173f5f)
         .setFooter('Aura Discord Bot | Developed By Varsp');
@@ -48,6 +48,8 @@ bot.on('message', message => {
         const moderation = new Discord.MessageEmbed()
                .setTitle('Moderator Commands')
                .setThumbnail('https://images-ext-2.discordapp.net/external/RyBiRwUtq8VqdDb6Sm77J303UEziO55Ujqu8FaRCfQ4/https/i.imgur.com/JsgxK3Y.png?width=160&height=160')
+               .setColor(0x173f5f)
+               .setFooter('Aura Discord Bot | Developed By Varsp');
                 message.channel.send(moderation);
     }
 })
