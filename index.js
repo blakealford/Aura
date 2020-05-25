3c555ea5da376444898375e1c0a69d429c79cce4
VarspTheDev <65226823+VarspTheDev@users.noreply.github.com>
2020-05-25 21:37:02 +1000

    d

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ bot.on('message', message => {
      var prefix = "-";
      var cont = message.content.slice(prefix.length).split(' '); 
      var args = cont.slice(1);
+    if(!message.content.startsWith('-')) return;
     switch(cont.toString()){
-    case 'help':
+    
+        case 'help':
         const help = new Discord.MessageEmbed()
         .setTitle('Aura Bot Commands')
+        .setThumbnail('https://media.discordapp.net/attachments/714301789352099862/714302588799156316/Aura-Logo.png?width=400&height=400')
         .addFields(
             {name: 'Moderation', value: '`-help moderation`', inline: true }, 
             {name: 'Reaction Roles', value:  '`-help Reaction`', inline: true }, 
             {name: 'Music', value: '`-help Music`', inline: true }, 
             {name: 'Commands', value: '`-help Commands`', inline: true }, 
+            {name: 'Extra Support', value: 'Join Our DiSscord - '}
         )  
         .setColor(0x173f5f)
         .setFooter('Aura Discord Bot | Developed By Varsp');
@@ -34,11 +38,17 @@ bot.on('message', message => {
 
         case 'varsp':
             const varsp = new Discord.MessageEmbed() 
-            .setTitle('He is a massive nerd and made the bot And You Should Follow Him On twitter @DeveloperVarsp')
+            .setTitle('He is a massive nerd and made the bot And You Should Follow Him On Twitter @DeveloperVarsp')
             .setColor(0x173f5f);
             message.channel.send(varsp);
 
-            break; 
+    break; 
+        
+    case 'moderation':
+        const moderation = new Discord.MessageEmbed()
+               .setTitle('Moderator Commands')
+               .setThumbnail('https://images-ext-2.discordapp.net/external/RyBiRwUtq8VqdDb6Sm77J303UEziO55Ujqu8FaRCfQ4/https/i.imgur.com/JsgxK3Y.png?width=160&height=160')
+                message.channel.send(moderation);
     }
 })
 
