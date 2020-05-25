4f6b2c53cb67f3018039ae69872020b461fee67a
VarspTheDev <65226823+VarspTheDev@users.noreply.github.com>
2020-05-25 19:02:57 +1000

    Aura Bot

---
+++ index.js
@@ -0,0 +1,46 @@
+const botconfig = require('./botconfig')
+
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+var version = ('1.0.0')
+
+bot.on('ready', () =>{
+    console.log('Aura Bot Is Now Online! Running Version ' + version);
+    bot.user.setActivity(`${bot.guilds.cache.size} servers | -help`,  { type: 'WATCHING' })                                                                                                                           
+});
+
+bot.on('msg', msg=> {
+
+    let args = msg.content.substring(botconfig.prefix.length).split(" ");
+
+    switch (args[0].toString()){
+
+    case 'help':
+        const help = new Discord.MesasgeEmbed()
+        .setTitle('Aura Bot Commands', 'https://media.discordapp.net/attachments/714301789352099862/714302588799156316/Aura-Logo.png?width=400&height=400')
+        .setThumbnail('https://media.discordapp.net/attachments/714301789352099862/714302588799156316/Aura-Logo.png?width=400&height=400')
+        .addFields(
+            {name: 'Moderation', value: '`-help moderation`', inline: true }, 
+            {name: 'Reaction Roles', value:  '`-help Reaction`', inline: true }, 
+            {name: 'Music', value: '`-help Music`', inline: true }, 
+            {name: 'Commands', value: '`-help Commands`', inline: true }, 
+        )  
+        .setColor(0x173f5f)
+        .setFooter('Aura Discord Bot | Developed By Varsp')
+        msg.channel.send(help);
+        
+    break; 
+
+        case 'varsp':
+            const varsp = new Discord.MessageEmbed() 
+            .setTitle('He is a massive nerd and made the bot')
+            .setColor(0x4457C1);
+            msg.channel.send(varsp);
+
+            break; 
+    }
+})
+
+bot.login(process.env.token);
