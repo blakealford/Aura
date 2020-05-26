c2c1fbf76423c455f4df8430c1556265b2a898de
VarspTheDev <65226823+VarspTheDev@users.noreply.github.com>
2020-05-26 11:50:00 +1000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -21,21 +21,7 @@ module.exports.run = async (bot, message, args) => {
             .setColor(0x173f5f)
             .setFooter('Aura Discord Bot | Developed By Varsp');
             message.channel.send(help);
-        }}
-        if(!args[0]) {
-            let help = new Discord.MessageEmbed()
-            .setTitle('Aura Bot Commands')
-            .setThumbnail('https://media.discordapp.net/attachments/714301789352099862/714302588799156316/Aura-Logo.png?width=400&height=400')
-            .addFields(
-                {name: 'Moderation', value: '`-help moderation`', inline: true }, 
-                {name: 'Reaction Roles', value:  '`-help Reaction`', inline: true }, 
-                {name: 'Music', value: '`-help Music`', inline: true }, 
-                {name: 'Commands', value: '`-help Commands`', inline: true }, 
-                {name: 'Extra Support', value: 'Join Our Discord - discord.gg/kwC5QAx'}
-            )
-            .setColor(0x173f5f)
-            .setFooter('Aura Discord Bot | Developed By Varsp');
-            message.cahnnel.send(help);
+        }
         }
 }
 
