f3bd49e90bd5e14076266bc40ed6697b6d5a02dc
VarspTheDev <65226823+VarspTheDev@users.noreply.github.com>
2020-07-04 12:15:40 +1000

    cc

--- commands/music/play.js
+++ commands/music/play.js
@@ -2,53 +2,63 @@ const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const colours = require("../JSON/colours.json");
 const bot = new Discord.Client();
+let musicConn;
+let musicChannel;
+let musicDispatcher;
+let queue = [];
+let npUrl ="";
   module.exports = {
   name: "play",
   category: "music",
   description: "Play a song",
   run: async (bot, message, args) => {
-    function playYoutubeURL(textChannel, voiceChannel, url)
-
-    voiceChannel.join().then((con) => {
-        let dispatcher = conn.play(ytdl(url, {filter: "audioonly", quality: "highestaudio"}));
-
-        textChannel.send(":speaker: **Now Playing**" + url+"in"+ voiceChannel)
-
-        dispatcher.con("speaking", (speaking) => {
-            if(!speaking){
-                textChannel.send(":speaker: **Song has ended**, disconetcing from "+voiceChannel)
-                conn.disoonect();
-            }
-        })
-    });
-
+    
   let url = args[0];
   if(!url) {
-      return channel.send("Please provide a URL")
+      return message.channel.send("Please provide a URL")
   }
 
   let voiceChannel = message.member.voice.channel;
   if(!voiceChannel) {
-  return channel.send(":speaker: **Join a voice channel to play music**")
+  return message.channel.send(":speaker: **Join a voice channel to play music**")
   }
 
-  let inVoiceChannel = bot.user.voice && bot.user.voice.channel;
-  if(inVoiceChannel) {
-      return channel.send(":speaker: **I am already play music!**")
-  }
+ if(musicConn && musicConn.channel.id !== voiceChannel.id) {
+     return message.channel.send(":speaker: **I am already play music!**")
+ } 
 
-  playYoutubeURL(channel, voiceChannel, url)
-}}
+ queue.push(url);
+
+ if(!musicConn) {
+     voiceChannel.join().then(conn => {
+         musicConn = conn;
+         playNextSong();
+     })
+ }
+
+  playNextSong(voiceChannel, url)
+  function playNextSong(){
+
+    voiceChannel.join().then((conn) => {
+       let nextUrl = queue[0];
+        musicDispatcher = musicConn.play(ytdl(nextUrl, {filter: "audioonly", quality: "highestaudio"}));
+
+        message.channel.send(":speaker: **Now Playing**" + url+"in"+ voiceChannel)
+
+        musicDispatcher.on("speaking", (speaking) => {
+            if(!speaking){
+
+                if(queue.length === 0) {
+                    message.channel.send(":speaker: **Song has ended**, disconetcing from "+voiceChannel)
+                    musicConn.disconnect();
+                    musicConn = null;
+                } else {
+                    playNextSong();
+                }
+            }
+        })
+    });
 
 
-/*const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
-const colours = require("../JSON/colours.json");
-const bot = new Discord.Client();
-module.exports = {
-  name: "play",
-  category: "music",
-  description: "Play a song",
-  run: async (bot, message, args) => {
-  
-}}*/
+
+}}}
---
+++ commands/music/stop.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const colours = require("../JSON/colours.json");
+const bot = new Discord.Client();
+let musicConn;
+let musicChannel;
+let musicDispatcher;
+let queue = [];
+let npUrl ="";
+  module.exports = {
+  name: "stop",
+  category: "music",
+  description: "Stop a song",
+  run: async (bot, message, args) => {
+    musicConn.disconnect();
+    musicConn = null;
+    queue = [];            
+
+    return message.channel.send("I have left the Voice Channel")
+
+}}
